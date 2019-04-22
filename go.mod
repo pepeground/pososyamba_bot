570f0cecc9b0469c223e53d7c04c81d5ac38e098
Artur Borisovich <thesunwave@yandex.ru>
2019-04-22 14:36:29 +0300

    Add counting gay id

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/brunopita/go-common v0.0.0-20181128165638-9bc7e4da8eb3 // indirect
 	github.com/brunopita/influxdb-client v0.0.0-20190110191440-53e3b449c706 // indirect
+	github.com/go-redis/redis v6.15.2+incompatible
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/influxdata/influxdb v1.7.5 // indirect
 	github.com/influxdata/influxdb1-client v0.0.0-20190402204710-8ff2fc3824fc
--- pososyamba_bot.go
+++ pososyamba_bot.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"github.com/go-redis/redis"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/influxdata/influxdb1-client/v2"
 	"log"
 	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -23,6 +26,26 @@ var preparePhrases = []string{
 }
 
 var mainPososyamba = "ПОСОСЯМБА"
+
+var gayName = []string{
+	"faggot",
+	"pidor",
+	"mraz",
+	"petushok",
+}
+
+var gayAdjective = []string{
+	"pretty",
+	"fat",
+	"cool",
+	"sweety",
+}
+
+var redisdb = redis.NewClient(&redis.Options{
+	Addr:     "redis:6379", // use default Addr
+	Password: "",           // no password set
+	DB:       0,            // use default DB
+})
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
@@ -56,7 +79,7 @@ func main() {
 
 			query := update.InlineQuery
 
-			go sendToInflux(query.From.UserName, query.From.ID, 0, "", "inline")
+			go sendToInflux(query.From.UserName, query.From.ID, 0, "", "inline", "inline")
 
 			_, err := bot.AnswerInlineQuery(inlineConf)
 
@@ -90,15 +113,80 @@ func main() {
 			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "")
 			msg.Text = preparePhrases[rand.Intn(len(preparePhrases))]
 
-			go sendToInflux(message.From.UserName, message.From.ID, message.Chat.ID, message.Chat.Title, "message")
+			go sendToInflux(message.From.UserName, message.From.ID, message.Chat.ID, message.Chat.Title, "message", "pososyamba")
 
 			sendMessage(msg, bot)
 
 			msg.Text = buildPososyamba()
 
 			sendMessage(msg, bot)
+		case "gay_id":
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+			forwardedMessage := update.Message
+
+			var gayID, username string
+			var clientID int
+			var err error
+
+			if forwardedMessage.ReplyToMessage != nil {
+				username = forwardedMessage.ReplyToMessage.From.UserName
+				clientID = forwardedMessage.ReplyToMessage.From.ID
+				gayID, err = redisdb.Get(strconv.Itoa(clientID)).Result()
+
+				log.Println(clientID)
+				msg.ReplyToMessageID = forwardedMessage.ReplyToMessage.MessageID
+				log.Println(gayID)
+			} else {
+				username = forwardedMessage.From.UserName
+				clientID = forwardedMessage.From.ID
+				gayID, err = redisdb.Get(strconv.Itoa(clientID)).Result()
+				log.Println(clientID)
+				log.Println(gayID)
+			}
+
+			if err != nil {
+				msg.Text = generateGayID()
+
+				err := redisdb.Set(strconv.Itoa(clientID), msg.Text, 0).Err()
+
+				if err != nil {
+					log.Println(err)
+				}
+			} else {
+				msg.Text = gayID
+			}
+
+			msg.Text = "@" + username + " has gay_id: #" + msg.Text
+
+			go sendToInflux(message.From.UserName, message.From.ID, message.Chat.ID, message.Chat.Title, "message", "gay_id")
+
+			sendMessage(msg, bot)
+
+		case "renew_gay_id":
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+			gayID := generateGayID()
+
+			msg.Text = "@" + update.Message.From.UserName + " you have updated gay_id: #" + gayID
+
+			err := redisdb.Set(strconv.Itoa(update.Message.From.ID), gayID, 0).Err()
+
+			if err != nil {
+				log.Println(err)
+			}
+
+			go sendToInflux(message.From.UserName, message.From.ID, message.Chat.ID, message.Chat.Title, "message", "renew_gay_id")
+
+			sendMessage(msg, bot)
 		}
 	}
+}
+
+func generateGayID() string {
+	return strings.Join(
+		[]string{gayAdjective[rand.Intn(len(gayAdjective))], gayName[rand.Intn(len(gayName))], fmt.Sprintf("%v", rand.Intn(10000))}, "_",
+	)
 }
 
 func sendMessage(msg tgbotapi.MessageConfig, bot *tgbotapi.BotAPI) {
@@ -117,7 +205,7 @@ func buildPososyamba() string {
 	return strings.Join(text, "\r\n\n")
 }
 
-func sendToInflux(username string, userID int, chatID int64, chatTitle, messageType string) {
+func sendToInflux(username string, userID int, chatID int64, chatTitle, messageType, command string) {
 	c, err := client.NewHTTPClient(client.HTTPConfig{
 		Addr:     os.Getenv("INFLUX_URL"),
 		Username: os.Getenv("INFLUX_USERNAME"),
@@ -134,7 +222,7 @@ func sendToInflux(username string, userID int, chatID int64, chatTitle, messageT
 	})
 
 	// Create a point and add to batch
-	tags := map[string]string{"telegram": "pososyamba"}
+	tags := map[string]string{"command": command}
 	fields := map[string]interface{}{
 		"username":    username,
 		"user_id":     userID,
@@ -147,8 +235,11 @@ func sendToInflux(username string, userID int, chatID int64, chatTitle, messageT
 	if err != nil {
 		log.Println("Error: ", err.Error())
 	}
-	bp.AddPoint(pt)
 
-	// Write the batch
-	c.Write(bp)
+	if os.Getenv("ENVIRONMENT") == "production" {
+		bp.AddPoint(pt)
+
+		// Write the batch
+		c.Write(bp)
+	}
 }
