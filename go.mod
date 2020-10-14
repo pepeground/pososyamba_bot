d3e814522ea66681780a84200f45a10c55df6c6c
Artur Borisovich <thesunwave@users.noreply.github.com>
2020-10-15 00:09:03 +0300

    Add F video notes #48 (#54)

--- go.mod
+++ go.mod
@@ -18,4 +18,5 @@ require (
 	golang.org/x/net v0.0.0-20190603091049-60506f45cf65 // indirect
 	golang.org/x/sys v0.0.0-20190606165138-5da285871e9c // indirect
 	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/yaml.v2 v2.2.2
 )
--- internal/app/bot/bot_client.go
+++ internal/app/bot/bot_client.go
@@ -8,12 +8,14 @@ import (
 	"github.com/thesunwave/pososyamba_bot/configs"
 	"github.com/thesunwave/pososyamba_bot/internal/app/admin"
 	"github.com/thesunwave/pososyamba_bot/internal/app/analytics"
+	"github.com/thesunwave/pososyamba_bot/internal/app/cache"
 	"github.com/thesunwave/pososyamba_bot/internal/app/commands"
 	"github.com/thesunwave/pososyamba_bot/internal/app/fakenews"
-	"github.com/thesunwave/pososyamba_bot/internal/app/string_builder"
 	"github.com/thesunwave/pososyamba_bot/internal/app/mrkshi"
+	"github.com/thesunwave/pososyamba_bot/internal/app/string_builder"
 	"gopkg.in/yaml.v2"
-	
+	"time"
+
 	"io/ioutil"
 	"math/rand"
 	"os"
@@ -44,7 +46,7 @@ func Init() {
 	if err != nil {
 		log.Error().Err(err).Msg("")
 	}
-	
+
 	err = yaml.Unmarshal(file, &mrkshi_phrases)
 	if err != nil {
 		log.Error().Err(err).Msg("")
@@ -77,14 +79,14 @@ func (c BotClient) run() {
 
 		if update.ChannelPost != nil {
 			if update.ChannelPost.Text != "" { // if channel post is a plain text
-				go mrkshi.UpdatePhrases(update.ChannelPost.Text, &mrkshi_phrases)			
+				go mrkshi.UpdatePhrases(update.ChannelPost.Text, &mrkshi_phrases)
 				continue
 			}
 
 			if update.ChannelPost.Caption != "" { // if channel post is a photo with caption
 				go mrkshi.UpdatePhrases(update.ChannelPost.Caption, &mrkshi_phrases)
 				continue
-			}	
+			}
 
 		}
 
@@ -167,8 +169,10 @@ func messageCommandHandler(update *tgbotapi.Update, botClient *BotClient) {
 		messages = adminHandlers.FlushHotNews()
 	case "hot_news":
 		messages = handlers.HotNews()
-	case "f", "F":
+	case "f":
 		messages = handlers.F()
+	case "F":
+		messages = handlers.NewF()
 	case "MRKSHI", "mrkshi":
 		messages = handlers.MRKSHI(&mrkshi_phrases)
 	default:
@@ -194,6 +198,18 @@ func (c *BotClient) sendMessage(messages interface{}) {
 			if _, err := c.Bot.Send(message); err != nil {
 				log.Fatal().Err(err).Msg("Something went wrong")
 			}
+		}
+	}
+
+	videoNoteMessages, ok := messages.(*[]tgbotapi.VideoNoteConfig)
+	if ok {
+		for _, message := range *videoNoteMessages {
+			msg, err := c.Bot.Send(message)
+
+			if err != nil {
+				log.Fatal().Err(err).Msg("Something went wrong")
+			}
+			cache.Redis().Set("funeral_video_id", msg.VideoNote.FileID, 100*time.Hour)
 		}
 	}
 }
